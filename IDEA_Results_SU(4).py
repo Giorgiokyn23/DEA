87b922863ad7299a1f34d9b98b08f1ccb47a7e07
Yogev_173 <pirina.97@outlook.it>
2024-10-25 18:47:12 +0200

    Added Analysis of SU(4) of the convolutional layer
    - there is a bug since it gave 0
    - needs debugging

---
+++ IDEA_Results_SU(4).py
@@ -0,0 +1,45 @@
+from DEA import Circuit, DEA
+
+
+C = Circuit(2)
+C.apply(C.RZ(1))       # RZ gate on qubit1
+C.apply(C.CNOT(0, 1))  # CNOT between qubit0 and qubit1
+C.apply(C.RZ(0))       # RZ gate on qubit0
+C.apply(C.RX(0))       # RX gate on qubit0
+C.apply(C.RZ(0))       # RZ gate on qubit0
+C.apply(C.RZ(1))       # RZ gate on qubit1
+C.apply(C.RX(1))       # RX gate on qubit1
+C.apply(C.RZ(1))       # RZ gate on qubit1
+C.apply(C.CNOT(0, 1))  # CNOT between qubit0 and qubit1
+C.apply(C.RX(0))       # RX gate on qubit0
+C.apply(C.RZ(1))       # RZ gate on qubit1
+C.apply(C.CNOT(0, 1))  # CNOT between qubit0 and qubit1
+C.apply(C.RZ(0))       # RZ gate on qubit0
+C.apply(C.RX(0))       # RX gate on qubit0
+C.apply(C.RZ(0))       # RZ gate on qubit0
+C.apply(C.RZ(1))       # RZ gate on qubit1
+C.apply(C.RX(1))       # RX gate on qubit1
+C.apply(C.RZ(1))
+
+phi_dict = {
+    0: [0],
+    1: [1],
+    2: [2],
+    3: [3],
+    4: [4],
+    5: [5],
+    6: [6],
+    7: [7],
+    8: [8],
+    9: [9],
+    10: [10],
+    11: [11],
+    12: [12],
+    13: [13],
+    14: [14],
+}
+
+unique1, indep_params1 = DEA(C,n_points=100,tol=10**(-13))
+print(unique1,indep_params1)
+if unique1:
+    print("The list of independent parameters is "+str(indep_params1)+".")
